63d072a8b9b6a7f8a3a69b0f8cb10d8bfeddec3b
Julian <yizman@list.ru>
2025-09-23 12:39:34 +0300

    upd task1

--- task1/Main.java
+++ task1/Main.java
@@ -3,9 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         Features exercise1 = new Features();
-        exercise1.drawFigures();
-
-        
+        exercise1.drawSquare(9);
+        exercise1.drawOval(9);
+        exercise1.drawPointer(9);
+        exercise1.drawRhombus(9);
     }
 
 }
