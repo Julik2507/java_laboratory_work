61a147e52f261a622eb9f95cb6e8de9622aa503a
Julian <yizman@list.ru>
2025-10-07 20:49:16 +0300

    upd: add table format

--- task1/Math.java
+++ task1/Math.java
@@ -8,16 +8,20 @@ public class Math {
         return total;
     }
 
-    public long calcFactorial() {
+    public void calcFactorial() {
+        System.out.println("+------+----------------------------+");
+        System.out.printf("| %-4s | %-26s |%n", "n", "n!");
+        System.out.println("+------+----------------------------+");
+
         long total = 1;
         for(int i = 1; i <= 20; i++) {
             for(int j = 2; j <= i; j++) {
                 total *= j;
             }
-            System.out.printf("Factorial %d = %d\n", i, total);
+            System.out.printf("| %-4d | %-26d |%n", i, total);
             total = 1;
         }
-        return total;
+        System.out.println("+------+----------------------------+");
     }
 
 
