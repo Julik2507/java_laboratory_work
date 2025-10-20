2602ffd4976532435a69e18112812ec52c586e52
Julian <yizman@list.ru>
2025-10-20 19:34:20 +0300

    add: complete first task

---
+++ task1/Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean status = true;
+        int startBound = 1;
+        int finishBound = 1000;
+        
+        Random random = new Random();
+        Scanner myScanner = new Scanner(System.in);
+       
+        int secretValue = random.nextInt(finishBound - startBound +1) + startBound;
+        System.out.println(secretValue);
+        while(status) {
+            System.out.println("Угадайте число от 1 до 1000 (введите 0 для выхода):");
+            int value = myScanner.nextInt();
+            if(value == 0) break;
+
+            if(value < secretValue) {
+                System.out.println("Загаданное число больше!");
+            } else if(value > secretValue) {
+                System.out.println("Загаданное число меньше!");
+            } else if(value == secretValue) {
+                System.out.println("Поздравляю! Вы угадали число!"); 
+                status=false;
+            }
+        }
+
+    }
+
+}
